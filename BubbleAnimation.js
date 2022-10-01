51d605f5c6da7940870dfae7df0a11a824e002d9
Aninha-Moraes <anamoraes0302@gmail.com>
2022-10-01 20:58:28 -0300

    First commit

---
+++ BubbleAnimation.js
@@ -0,0 +1,88 @@
+class BubbleAnimation{
+    #__canvas = null;
+    #__canvasPointer = null;
+    #__colorsPointer = null;
+    #__bubblesArray = [];
+    #__maxBubbleSize = null;
+    #__bubbleGrowingStep = null;
+    #__bubbleCreationRate = null;
+    #__userIsOnline = true;
+    #__bubblesBackground = null;
+
+    constructor(canvas, colors, maxBubbleSize, bubbleGrowingStep, bubbleCreationRate, backgroundColor){
+        this.#__canvasPointer = canvas.getContext("2d");
+        this.#__colorsPointer = colors;
+        this.#__maxBubbleSize = maxBubbleSize;
+        this.#__bubbleGrowingStep = bubbleGrowingStep;  
+        this.#__bubbleCreationRate = bubbleCreationRate;
+        this.#__bubblesBackground = backgroundColor
+        this.#__canvas = canvas;
+
+        this.bubbleAnimation = this.bubbleAnimation.bind(this);
+
+        document.addEventListener("visibilitychange", () => {
+            switch(this.#__userIsOnline){
+                case true:
+                    this.#__userIsOnline = false;
+                    break;
+                case false:
+                    this.#__userIsOnline = true;
+                    break;
+            }
+        });
+
+
+        setInterval(() => {
+            if(this.#__userIsOnline == true){
+                this.#__bubblesArray.push({
+                    "growing" : true,
+                    "color" : this.#__colorsPointer[Math.trunc(Math.random() * this.#__colorsPointer.length)],
+                    "currentSize" : 0,
+                    "x" : Math.trunc(Math.random() * canvas.width),
+                    "y" : Math.trunc(Math.random() * canvas.height)
+                })
+            }
+        }, this.#__bubbleCreationRate);
+
+        window.requestAnimationFrame(this.bubbleAnimation);
+    }
+
+    bubbleAnimation(){
+        this.#__canvasPointer.beginPath();
+        this.#__canvasPointer.fillStyle = this.#__bubblesBackground;
+        this.#__canvasPointer.rect(0,0,this.#__canvas.width, this.#__canvas.height);
+        this.#__canvasPointer.fill();
+        this.#__canvasPointer.closePath();
+
+        for(let i = 0; i < this.#__bubblesArray.length; i++){
+            switch(this.#__bubblesArray[i].growing){
+                case true:
+                    if(this.#__bubblesArray[i].currentSize >= this.#__maxBubbleSize){
+                        this.#__bubblesArray[i].growing = false;
+                    }
+                    else{
+                        this.#__bubblesArray[i].currentSize += this.#__bubbleGrowingStep;
+                    }
+                    break;
+                case false:
+                    if(this.#__bubblesArray[i].currentSize <= 0 || this.#__bubblesArray[i].currentSize - this.#__bubbleGrowingStep <= 0){
+                        this.#__bubblesArray.splice(i,1);
+                    }
+                    else{
+                        this.#__bubblesArray[i].currentSize -= this.#__bubbleGrowingStep;
+                    }
+                    break;
+            }
+        }
+
+        for(const bubble of this.#__bubblesArray){
+            this.#__canvasPointer.beginPath();
+            this.#__canvasPointer.fillStyle = bubble.color;
+            this.#__canvasPointer.arc(bubble.x, bubble.y, bubble.currentSize, 0, 2 * Math.PI, false);
+            this.#__canvasPointer.fill();
+            this.#__canvasPointer.closePath();
+        }
+
+        window.requestAnimationFrame(this.bubbleAnimation);
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+<div align="center">
+    <h1>Bubble background</h1>
+    <img height="250px" src="./assets/BubbleDemonstration.png">
+</div>
+
+<br/>
+
+# What's that
+Bubble background is a tiny library for make a bubble effect in the canvas, the code is resumed in a class that just need be instantiated receiving the correct arguments. The bubble effect is like in the image above, circles growing up and shirinking until disappear from canvas, the background color, bubble's color, bubble's radius and bubble's spawn rate is programmable.
+
+
+# How to use
+First of all, you need import the BubbleAnimation.js to your .html file and then run the following commands in a script above:
+
+```html
+    <!--Importing the animation file-->
+    <script src="./BubbleAnimation.js"></script>
+    <script>
+        // Get canvas element
+        let canvas = document.getElementById("target_canvas");
+
+        /* 
+            You should define the canvas size here,
+            if the canvas's CSS is defined as dynamic 
+            size like using '%' you could use 'getComputedStyle'
+            to redefine these variables every time that be
+            changed
+        */ 
+        canvas.width = 500;
+        canvas.height = 500;
+
+        /* Call the animation class properly, passing
+        the canvas element, all possible colors of bubbles,
+        max bubble's radius, growing step of bubbles, bubbles's
+        spawn rate and then the background color*/
+        new BubbleAnimation(canvas, ["black", "purple", "gray", "blue", "red"], 30, 0.2, 600, "darkslateblue");
+
+        // The program should run now :)
+    </script>
+```
